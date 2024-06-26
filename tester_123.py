ca139cfc2b01035391de0bd5268753815688f30a
Demo12-droid <ibi632024@gmail.com>
2024-06-26 17:05:00 +0530

    Update tester_123.py

--- tester_123.py
+++ tester_123.py
@@ -31,11 +31,10 @@ message_history = st.empty()
 
 if st.session_state['user_input']:
 	for i in range(0 , len(st.session_state['user_input']) - 1, 1):
-		# This function displays user input
-		message(st.session_state["user_input"][i], 
-				key=str(i),avatar_style="icons")
 		# This function displays OpenAI response
 		message(st.session_state['response'][i], 
 				avatar_style="miniavs",is_user=True,
 				key=str(i) + 'data_by_user')
-
+		# This function displays user input
+		message(st.session_state["user_input"][i], 
+				key=str(i),avatar_style="icons")
