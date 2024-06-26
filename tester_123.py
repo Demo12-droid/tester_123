e35e04c1b4858da9e87ad52dede497a3daab67d8
Demo12-droid <ibi632024@gmail.com>
2024-06-26 17:30:55 +0530

    Update tester_123.py

--- tester_123.py
+++ tester_123.py
@@ -45,8 +45,3 @@ with col2:
     st.write("Chat Input:")
     content = st.chat_input(key='content', on_submit=chat_content)
 
-    # Floating button setup
-    button_b_pos = "1rem"
-    button_css = float_css_helper(width="3rem", bottom=button_b_pos, right="1rem", transition=0.2)
-    float_parent(css=button_css)
-
