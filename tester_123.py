4b3a2d177e0338a91b89064b3c17ec1f75af2a9f
Demo12-droid <ibi632024@gmail.com>
2024-06-26 17:03:11 +0530

    Update tester_123.py

--- tester_123.py
+++ tester_123.py
@@ -30,7 +30,7 @@ if user_input:
 message_history = st.empty()
 
 if st.session_state['user_input']:
-	for i in range(len(st.session_state['user_input']) - 1, -1, -1):
+	for i in range(0 , len(st.session_state['user_input']) - 1, 1):
 		# This function displays user input
 		message(st.session_state["user_input"][i], 
 				key=str(i),avatar_style="icons")
