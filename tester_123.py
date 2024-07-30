1c4633c60820b88b5961108773e2bef7bba2c9d3
Demo12-droid <ibi632024@gmail.com>
2024-07-30 12:51:44 +0530

    Update tester_123.py

--- tester_123.py
+++ tester_123.py
@@ -29,7 +29,8 @@ def get_session_ids(username):
 # Function to generate a new session ID
 def generate_new_session_id(username):
     new_session_id = str(uuid.uuid4())
-    users_db["username"]["session_ids"].append(new_session_id)
+    user = users_db.get(username)
+    user["session_ids"].append(new_session_id)
     return new_session_id
 
 def logout():
