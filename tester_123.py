974c50be7fb6a39ec82c49cc9ef896a7e3ac9a75
Demo12-droid <ibi632024@gmail.com>
2024-07-30 13:13:37 +0530

    Update tester_123.py

--- tester_123.py
+++ tester_123.py
@@ -111,74 +111,74 @@ else:
 		
 	
 
-
-# Streamlit app
-st.title("Chanakya")
-
-
-st.sidebar.title("Options")
-
-st.sidebar.header("Database options")
-toggle_option = st.sidebar.selectbox(
-    'Choose a Database:',
-    ['congestion', 'toll_plaza_data']
-)
-
-st.sidebar.header("Display Options")
-show_plot = st.sidebar.checkbox("Plot",value=True)
-
-if st.sidebar.button("Logout"):
-    logout()
-    st.experimental_rerun() 
-
-user_input = st.chat_input("Ask a question...")
-
-if 'messages' not in st.session_state:
-    st.session_state.messages = []
-
-if user_input:
-    st.session_state.messages.append({"role": "user", "content": user_input})
-    sql, df, text_summary, plot, time_taken = get_response(user_input,show_plot,toggle_option)
-
-    # df = df.to_dict(orient='records') if isinstance(df, pd.DataFrame) else df
-    st.session_state.messages.append({
-        "role": "assistant",
-        "content": {
-            "user_input": user_input,
-            "sql": sql,
-            "df": df,
-            "text_summary": text_summary,
-            "plot": plot,
-            "time_taken": time_taken 
-        }
-    })
-
-for entry in st.session_state.messages:
-    role = entry.get('role', 'unknown role')
-    content = entry.get('content', {})
-
-    if role == 'user':
-        with st.chat_message("User"):
-            st.write(content)
-    elif role == 'assistant':
-        sql_query = content.get('sql', None)
-        text_summary = content.get('text_summary', None)
-        plot = content.get('plot', None)
-        df = content.get('df', None) 
-        time_taken = content.get('time_taken')
-
-        if sql_query is not None and df is None:
-            with st.chat_message("assistant"):
-                st.write("No data is available for the given question.If data is available, please retry")
-        else:            
-            with st.chat_message("assistant"):
-                if df:
-                    st.dataframe(df)
-                if text_summary:
-                    st.write(text_summary)
-                if plot:
-                    try:
-                        display_plot(plot)
-                    except:
-                        components.html(plot,height=390,scrolling=True)
-                st.write(f"<b>Time taken: {time_taken:.4f} seconds</b>", unsafe_allow_html=True)
+if st.session_state.logged_in:
+	# Streamlit app
+	st.title("Chanakya")
+	
+	
+	st.sidebar.title("Options")
+	
+	st.sidebar.header("Database options")
+	toggle_option = st.sidebar.selectbox(
+	    'Choose a Database:',
+	    ['congestion', 'toll_plaza_data']
+	)
+	
+	st.sidebar.header("Display Options")
+	show_plot = st.sidebar.checkbox("Plot",value=True)
+	
+	if st.sidebar.button("Logout"):
+	    logout()
+	    st.experimental_rerun() 
+	
+	user_input = st.chat_input("Ask a question...")
+	
+	if 'messages' not in st.session_state:
+	    st.session_state.messages = []
+	
+	if user_input:
+	    st.session_state.messages.append({"role": "user", "content": user_input})
+	    sql, df, text_summary, plot, time_taken = get_response(user_input,show_plot,toggle_option)
+	
+	    # df = df.to_dict(orient='records') if isinstance(df, pd.DataFrame) else df
+	    st.session_state.messages.append({
+	        "role": "assistant",
+	        "content": {
+	            "user_input": user_input,
+	            "sql": sql,
+	            "df": df,
+	            "text_summary": text_summary,
+	            "plot": plot,
+	            "time_taken": time_taken 
+	        }
+	    })
+	
+	for entry in st.session_state.messages:
+	    role = entry.get('role', 'unknown role')
+	    content = entry.get('content', {})
+	
+	    if role == 'user':
+	        with st.chat_message("User"):
+	            st.write(content)
+	    elif role == 'assistant':
+	        sql_query = content.get('sql', None)
+	        text_summary = content.get('text_summary', None)
+	        plot = content.get('plot', None)
+	        df = content.get('df', None) 
+	        time_taken = content.get('time_taken')
+	
+	        if sql_query is not None and df is None:
+	            with st.chat_message("assistant"):
+	                st.write("No data is available for the given question.If data is available, please retry")
+	        else:            
+	            with st.chat_message("assistant"):
+	                if df:
+	                    st.dataframe(df)
+	                if text_summary:
+	                    st.write(text_summary)
+	                if plot:
+	                    try:
+	                        display_plot(plot)
+	                    except:
+	                        components.html(plot,height=390,scrolling=True)
+	                st.write(f"<b>Time taken: {time_taken:.4f} seconds</b>", unsafe_allow_html=True)
