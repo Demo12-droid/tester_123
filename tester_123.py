d9edb389edb0454d9f362431d834e7f146e5cc3d
Demo12-droid <ibi632024@gmail.com>
2024-07-30 13:47:06 +0530

    Update tester_123.py

--- tester_123.py
+++ tester_123.py
@@ -49,6 +49,7 @@ if 'show_message' not in st.session_state:
 if not st.session_state.logged_in:
 	st.title("Login")
 	username = st.text_input("Username")
+	password = st.text_input("Password", type="password")
 	if authenticate(username, password):
 		st.session_state.logged_in = True
 		st.session_state.username = username
