5ac068fc7bfe8f0e806f841341a942bf906ea2df
Demo12-droid <ibi632024@gmail.com>
2024-07-31 12:55:33 +0530

    Update tester_123.py

--- tester_123.py
+++ tester_123.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 import json
-from streamlit_float import * 
+from streamlit_float import *
 import time 
 
 float_init(theme=True, include_unstable_primary=False)
